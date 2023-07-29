b37cc6e7633cea71ba1ae0e382e82e6b6bcf33c8
connie <conniechen1619@gmail.com>
2023-07-29 11:23:50 +0800

    cifar resnet training and inference

--- Transfer-Learning/resnet.py
+++ Transfer-Learning/resnet.py
@@ -8,9 +8,9 @@ from torch.utils.data import DataLoader
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # hyper parameters
-epoch = 2
+epochs = 2
 batch_size = 64
-learning_reate = 1e-4  # 0.0001
+learning_rate = 1e-4  # 0.0001
 
 transform = transforms.Compose(
     [transforms.Resize([224, 224]),
@@ -22,4 +22,44 @@ train_dataset = torchvision.datasets.CIFAR10(root='./data', train=True, download
 test_dataset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
 train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+resnet50 = torchvision.models.resnet50(weights="IMAGENET1K_V2")
+resnet50.fc = nn.Linear(in_features=2048, out_features=10, bias=True)
+
+resnet50 = resnet50.to(device=device)
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(resnet50.parameters(), lr=learning_rate)
+n_total_steps = len(train_loader)
+for epoch in range(epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = resnet50(images)
+        # loss
+        loss = criterion(outputs, labels)
+        # backward
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print(f'Epoch [{epoch + 1}/{epoch}], Step [{i + 1}/{n_total_steps}], Loss: {loss.item():.4f}')
+
+with torch.no_grad():
+    n_correct = 0
+    n_samples = 0
+    n_class_correct = [0 for i in range(10)]
+    n_class_samples = [0 for i in range(10)]
+    for images, labels in test_loader:
+        images = images.to(device)
+        labels = labels.to(device)
+        outputs = resnet50(images)
+        _, predicted = torch.max(outputs, 1)
+        n_samples += labels.size(0)
+        n_correct += (predicted == labels).sum().item()
+
+    acc = 100.0 * n_correct / n_samples
+    print(f'Accuracy of the network: {acc} %')
